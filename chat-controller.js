3abecbe7a310dc629dbefe37cbd51d2374ee8ad1
jweb06 <129250640+jweb06@users.noreply.github.com>
2024-06-22 10:59:22 +0900

    Update chat-controller.js

--- chat-controller.js
+++ chat-controller.js
@@ -8,8 +8,8 @@ db.serialize(() => {
 
 function searchChatHistory(searchTerm) {
   return new Promise((resolve, reject) => {
-    const query = `SELECT * FROM chat_history WHERE message LIKE '%${searchTerm}%'`;
-    db.all(query, [], (err, rows) => {
+    const query = `SELECT * FROM chat_history WHERE message LIKE ?`;
+    db.all(query, [`%${searchTerm}%`], (err, rows) => {
       if (err) reject(err);
       resolve(rows);
     });
